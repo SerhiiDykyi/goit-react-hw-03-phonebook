c65bb3991672c686ddbba7e0061f7dfa4d4177c2
Serhii Dykyi <vankyverd@gmail.com>
2020-07-04 13:42:51 +0300

    add form

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
 import React, { Component } from 'react';
-// import shortid from 'shortid';
+import shortid from 'shortid';
 import Wrapper from './components/Wrapper';
-
+import Form from './components/Form';
 class App extends Component {
-  state = {};
+  state = {
+    contacts: [],
+    filter: '',
+  };
+
+  addContact = ({ name, number }) => {
+    if (name === '') {
+      return alert('Please, fill out the form!');
+    }
+    const isExist = this.state.contacts.some(contact => contact.name === name);
+    if (isExist) {
+      return alert(`${name} is already in contacts`);
+    }
+    const contact = {
+      id: shortid.generate(),
+      name,
+      number,
+    };
+    this.setState(({ contacts }) => ({
+      contacts: [...contacts, contact],
+    }));
+  };
   render() {
     return (
       <Wrapper>
         <h1>Phonebook</h1>
+        <Form onAddContact={this.addContact} />
       </Wrapper>
     );
   }
---
+++ src/components/Form/Form.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import shortid from 'shortid';
+import './Form.scss';
+
+class Form extends Component {
+  state = {
+    name: '',
+    number: '',
+  };
+
+  nameInputId = shortid.generate();
+  namberInputId = shortid.generate();
+
+  handleChange = event => {
+    const { name, value } = event.currentTarget;
+    this.setState({ [name]: value });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    const { onAddContact } = this.props;
+    onAddContact(this.state);
+    this.reset();
+  };
+
+  reset = () => {
+    this.setState({ name: '', number: '' });
+  };
+
+  render() {
+    const { name, number } = this.state;
+    return (
+      <>
+        <form className="form_container" onSubmit={this.handleSubmit}>
+          <p>Name</p>
+          <label htmlFor={this.nameInputId}>
+            <input
+              type="text"
+              name="name"
+              value={name}
+              onChange={this.handleChange}
+            />
+          </label>
+          <p>Number</p>
+          <label htmlFor={this.namberInputId}>
+            <input
+              className="form_label"
+              type="tel"
+              name="number"
+              value={number}
+              onChange={this.handleChange}
+            />
+          </label>
+          <br />
+          <button className="button" type="submit">
+            Add contakt
+          </button>
+        </form>
+      </>
+    );
+  }
+}
+
+Form.propTypes = {
+  onAddContact: PropTypes.func.isRequired,
+};
+
+export default Form;
